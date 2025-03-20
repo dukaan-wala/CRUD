33ebaa633d674f3eed501bea9862bad46df8d537
dummydukaan <dummydukaan@gmail.com>
2025-03-20 23:35:54 +0530

    add comment

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,7 +2,7 @@ import express from "express";
 import { createUser, getUsers, getUserById, updateUser, deleteUser } from "../controllers/user.controller.js";
 
 const router = express.Router();
-       
+       //hhhhhhhhhhhhhhhhhh
 router.post("/", createUser);       // नया यूज़र जोड़ना
 router.get("/", getUsers);          // सभी यूज़र्स को पढ़ना
 router.get("/:id", getUserById);     // एक यूज़र पढ़ना
